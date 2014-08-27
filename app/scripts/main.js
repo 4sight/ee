80124ac6ca877897187c05e494fe74573d15d671
Greeley Dawson <hgd4th@gmail.com>
2014-08-27 11:58:01 -0400

    line breaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,15 @@
-$('div.link').mouseenter(function(){
+$('a.link').mouseenter(function(){
 	$(this).addClass('linkHover');
 });
-$('div.link').mouseleave(function(){
+$('a.link').mouseleave(function(){
   $(this).removeClass('linkHover');
 });
+$('img.linkedin').hover(function(){
+	$('.hiddenText').filter(':not(:animated)').animate({
+		opacity: 1
+	})},
+	function(){
+		$('.hiddenText').animate({
+			opacity: 0
+		})
+	});
