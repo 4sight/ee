738036debce30c2251240455e279a64ae455a769
4sight <hgd4th@gmail.com>
2018-04-06 17:12:10 -0400

    copied my homepage file structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,57 +1,8 @@
-$('a.link').mouseenter(function(){
-	$(this).addClass('linkHover');
+$(document).ready(function(){ 
+    $('#connect').mouseenter(function(){
+	  $(this).addClass('linkHover');
+	});
+	$('#connect').mouseleave(function(){
+	  $(this).removeClass('linkHover');
+	});
 });
-$('a.link').mouseleave(function(){
-  $(this).removeClass('linkHover');
-});
-$('a.resume').mouseenter(function(){
-	$(this).addClass('linkHover');
-});
-$('a.resume').mouseleave(function(){
-  $(this).removeClass('linkHover');
-});
-$('a#github').hover(function(){
-	$('#githubText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#githubText').animate({
-			opacity: 0
-		})
-	});
-$('a#linkedIn').hover(function(){
-	$('#linkedInText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#linkedInText').animate({
-			opacity: 0
-		})
-	});
-$('a#tumblr').hover(function(){
-	$('#tumblrText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#tumblrText').animate({
-			opacity: 0
-		})
-	});
-$('a#soundcloud').hover(function(){
-	$('#soundcloudText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#soundcloudText').animate({
-			opacity: 0
-		})
-	});
-$('.fa-phone').hover(function(){
-	$('#phoneText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#phoneText').animate({
-			opacity: 0
-		})
-	});
