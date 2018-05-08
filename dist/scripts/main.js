3617e40f72c85f5248fa1ac43a5d3ed134824cb9
4sight <hgd4th@gmail.com>
2018-05-08 18:17:51 -0400

    fixed dist folder to match app folder

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,48 +1,8 @@
-$('a.link').mouseenter(function(){
-	$(this).addClass('linkHover');
-});
-$('a.link').mouseleave(function(){
-  $(this).removeClass('linkHover');
-});
-$('a.resume').mouseenter(function(){
-	$(this).addClass('linkHover');
-});
-$('a.resume').mouseleave(function(){
-  $(this).removeClass('linkHover');
-});
-$('a#github').hover(function(){
-	$('#githubText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#githubText').animate({
-			opacity: 0
-		})
+$(document).ready(function(){ 
+    $('#connect').mouseenter(function(){
+	  $(this).addClass('linkHover');
 	});
-$('a#linkedIn').hover(function(){
-	$('#linkedInText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#linkedInText').animate({
-			opacity: 0
-		})
+	$('#connect').mouseleave(function(){
+	  $(this).removeClass('linkHover');
 	});
-$('a#tumblr').hover(function(){
-	$('#tumblrText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#tumblrText').animate({
-			opacity: 0
-		})
-	});
-$('.fa-phone').hover(function(){
-	$('#phoneText').filter(':not(:animated)').animate({
-		opacity: 1
-	})},
-	function(){
-		$('#phoneText').animate({
-			opacity: 0
-		})
-	});
+});
